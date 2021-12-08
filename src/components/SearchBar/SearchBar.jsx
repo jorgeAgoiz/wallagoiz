0421f998d907fd7f822df3120b9f9e110d9bd479
jorgeAP <jorgeagoiz@gmail.com>
2021-12-08 10:19:30 +0100

    Working on SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputBase from '@mui/material/InputBase'
-import { Box } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
-const styleProps = {
+const stylePropsBox = {
   gridColumn: '2 / 6',
   backgroundColor: '#d0e7b7',
   boxShadow: 4,
   borderRadius: 3,
   display: 'flex',
-  justifyContent: 'center'
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: 0.8,
+  width: '100%'
+}
+const stylePropsInputRoot = {
+  width: '50%'
+}
+const stylePropsInput = {
+  placeholder: 'Buscar...',
+  type: 'text',
+  name: 'search'
+}
+const stylePropsButton = {
+  backgroundColor: '#79955a'
 }
 
 const SearchBar = () => {
+  const [search, setSearch] = useState(null)
+
+  const onHandleChange = (evt) => {
+    setSearch(evt.target.value)
+  }
+
+  const onHandleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log(search)
+  }
+
   return (
-    <Box sx={styleProps}>
-      <InputBase placeholder='Buscar...' />
+    <Box sx={stylePropsBox} component='form' name='search-form' onSubmit={onHandleSubmit}>
+      <InputBase sx={stylePropsInputRoot} inputProps={stylePropsInput} onChange={onHandleChange} />
+      <Button variant='contained' size='small' type='submit' sx={stylePropsButton}>
+        Busqueda
+      </Button>
     </Box>
   )
 }
