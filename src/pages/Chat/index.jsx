544382719903933067153674c94a3c0f70486f58
jorgeAP <jorgeagoiz@gmail.com>
2022-02-06 16:57:41 +0100

    I dont know what chat type can I implement

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,44 +1,11 @@
-import React, { useState } from 'react'
-import { Grid } from '@mui/material'
-import { CHAT_ENGINE_PROJECTID, CHAT_ENGINE_SECRET, CHAT_ENGINE_USER } from '../../constants/index'
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
 import { styleProps } from './styles'
 
 const Chat = () => {
-  const [username, setUsername] = useState('')
-
-  const createDirectChat = (creds) => {
-    getOrCreateChat(
-      creds,
-      { is_direct_chat: true, usernames: [username] },
-      () => setUsername('')
-    )
-  }
-
-  const renderChatForm = (creds) => {
-    return (
-      <div>
-        <input
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button onClick={() => createDirectChat(creds)}>
-          Create
-        </button>
-      </div>
-    )
-  }
-
   return (
-    <Grid item xs={12} sm={10} md={8} marginTop={3} sx={styleProps}>
-      <ChatEngine
-        height='70vh'
-        projectID={CHAT_ENGINE_PROJECTID}
-        userName={CHAT_ENGINE_USER}
-        userSecret={CHAT_ENGINE_SECRET}
-        renderNewChatForm={(creds) => renderChatForm(creds)}
-      />
+    <Grid item xs={12} sm={10} md={10} marginTop={3} sx={styleProps}>
+      <Typography variant='h2'>Chat de contacto</Typography>
     </Grid>
 
   )
@@ -46,4 +13,4 @@ const Chat = () => {
 
 export default Chat
 
-/* Aquí hay mucho trabajo por hacer */
+/* Aqui implementaría un chat */
